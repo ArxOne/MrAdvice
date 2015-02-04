b37f8239b8d5a4a955f87fc8e16668bfc29a00c5
Pascal Craponne <pascal@craponne.fr>
2015-02-04 20:27:27 +0100

    Added information and signin

---
+++ ArxOne.Weavisor/Properties/ProductInfo.cs
@@ -0,0 +1,29 @@
+﻿#region Weavisor
+// Weavisor
+// A simple post build weaving package
+// https://github.com/ArxOne/Weavisor
+// Release under MIT license http://opensource.org/licenses/mit-license.php
+#endregion
+
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyCompany("Arx One")]
+[assembly: AssemblyProduct("Weavisor")]
+[assembly: AssemblyCopyright("MIT license http://opensource.org/licenses/mit-license.php")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
