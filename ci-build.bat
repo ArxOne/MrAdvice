c1c0b8c848e15d4ad905f5974103d8784ca87a1c
Pascal Craponne <pascal@craponne.fr>
2015-02-21 13:14:51 +0100

    Fix for ci-build

--- ci-build.bat
+++ ci-build.bat
@@ -1,4 +1,5 @@
 rem all of this because msbuild command line does not work well with packages restore
 
+set solution=MrAdvice.sln
 .nuget\nuget.exe restore %solution%
 msbuild %solution% /m /p:Configuration=Debug /p:Platform="Any CPU"
