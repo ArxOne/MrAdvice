e09b99d2c80b0bc6d318a554ade005b5cb4828f9
Pascal Craponne <pascal@craponne.fr>
2015-02-23 19:29:25 +0100

    Forgot to add a few files...

---
+++ Test/MethodLevelTest/Advices/InterfaceMethodAdvice.cs
@@ -0,0 +1,34 @@
+﻿#region Mr. Advice
+// Mr. Advice
+// A simple post build weaving package
+// http://mradvice.arxone.com/
+// Released under MIT license http://opensource.org/licenses/mit-license.php
+#endregion
+
+namespace MethodLevelTest.Advices
+{
+    using System;
+    using System.Reflection;
+    using ArxOne.MrAdvice.Advice;
+
+    public class InterfaceMethodAdvice : Attribute, IMethodAdvice
+    {
+        public int? NewReturnValue;
+        public int? NewFirstParameter;
+
+        public void Advise(MethodAdviceContext context)
+        {
+            // some non-sense mocking
+            var methodInfo = (MethodInfo)context.TargetMethod;
+            var returnType = methodInfo.ReturnType;
+            if (returnType != typeof(void))
+                context.ReturnValue = Activator.CreateInstance(returnType);
+
+            // now, some advice
+            if (NewFirstParameter.HasValue)
+                context.Parameters[0] = NewFirstParameter.Value;
+            if (NewReturnValue.HasValue)
+                context.ReturnValue = NewReturnValue.Value;
+        }
+    }
+}
---
+++ Test/MethodLevelTest/Advices/InterfacePropertyAdvice.cs
@@ -0,0 +1,27 @@
+﻿#region Mr. Advice
+// Mr. Advice
+// A simple post build weaving package
+// http://mradvice.arxone.com/
+// Released under MIT license http://opensource.org/licenses/mit-license.php
+#endregion
+namespace MethodLevelTest.Advices
+{
+    using System;
+    using ArxOne.MrAdvice.Advice;
+
+    public class InterfacePropertyAdvice : Attribute, IPropertyAdvice
+    {
+        public int? NewReturnValue;
+
+        public void Advise(PropertyAdviceContext context)
+        {
+            // some non-sense mocking
+            if (context.IsGetter)
+                context.ReturnValue = Activator.CreateInstance(context.TargetProperty.PropertyType);
+
+            // now, some advice
+            if (NewReturnValue.HasValue)
+                context.ReturnValue = NewReturnValue.Value;
+        }
+    }
+}
