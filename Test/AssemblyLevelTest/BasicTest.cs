121ad24f3762417e00ac8dcbf903c935c9f9e9a1
picrap <pascal@craponne.fr>
2015-04-08 17:59:23 +0200

    Added assembly-level tests

---
+++ Test/AssemblyLevelTest/BasicTest.cs
@@ -0,0 +1,32 @@
+﻿#region Mr. Advice
+// Mr. Advice
+// A simple post build weaving package
+// http://mradvice.arxone.com/
+// Released under MIT license http://opensource.org/licenses/mit-license.php
+#endregion
+
+using ExternalAdvices;
+
+[assembly: AssemblyAdvice]
+
+namespace AssemblyLevelTest
+{
+    using System.Reflection;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BasicTest
+    {
+        public void Advised()
+        {
+            var method = MethodBase.GetCurrentMethod();
+            Assert.AreNotEqual("Advised", method.Name);
+        }
+
+        [TestMethod]
+        public void AssemblyAdviceTest()
+        {
+            Advised();
+        }
+    }
+}
---
+++ Test/AssemblyLevelTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿#region Mr. Advice
+// Mr. Advice
+// A simple post build weaving package
+// http://mradvice.arxone.com/
+// Released under MIT license http://opensource.org/licenses/mit-license.php
+#endregion
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("AssemblyLevelTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("AssemblyLevelTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
