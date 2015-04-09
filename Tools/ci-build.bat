8967dfacc4c8d869f139ae7b29b893313572629d
picrap <pascal@craponne.fr>
2015-04-09 14:28:25 +0200

    Fixes for appveyor
    Yes, again

--- Tools/ci-build.bat
+++
@@ -1,5 +0,0 @@
-rem all of this because msbuild command line does not work well with packages restore
-
-set solution=MrAdvice.sln
-.nuget\nuget.exe restore %solution%
-msbuild %solution% /m /p:Configuration=Release /p:Platform="Any CPU"
