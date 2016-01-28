b7313db720fef9e665f35b8de90ef5affe70fce1
Pascal Craponne <pascal@craponne.fr>
2016-01-28 19:51:35 +0100

    A test assembly with Bcl

---
+++ Test/BclTest/WeavingTest.cs
@@ -0,0 +1,38 @@
+﻿#region Mr. Advice
+// Mr. Advice
+// A simple post build weaving package
+// http://mradvice.arxone.com/
+// Released under MIT license http://opensource.org/licenses/mit-license.php
+#endregion
+
+namespace BclTest
+{
+    using System;
+    using ArxOne.MrAdvice.Advice;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+    public class SomeAdvice : Attribute, IMethodAdvice
+    {
+        public void Advise(MethodAdviceContext context)
+        {
+            context.Proceed();
+            context.ReturnValue = (int)context.ReturnValue + 2;
+        }
+    }
+
+    [TestClass]
+    public class WeavingTest
+    {
+        [SomeAdvice]
+        public int F(int a) => a + 1;
+
+        [TestMethod]
+        [TestCategory("Weaving")]
+        public void BclAdviceTest()
+        {
+            var r = F(10);
+            Assert.AreEqual(13, r);
+        }
+    }
+}
