d022e0e03ca24e39559189f32b6c1f14c1160c1f
Pascal Craponne <pascal@craponne.fr>
2016-10-02 16:57:34 +0200

    Logging error

--- MrAdvice.Weaver/Weaver/AspectWeaver.cs
+++ MrAdvice.Weaver/Weaver/AspectWeaver.cs
@@ -201,6 +201,7 @@ namespace ArxOne.MrAdvice.Weaver
                     case ".NET 4.0":
                         return new TargetFramework(new Version(4, 0));
                     default:
+                        Logger.LogError($"Unknown RuntimeVersion: '{moduleDefinition.RuntimeVersion}'");
                         throw new ArgumentOutOfRangeException(moduleDefinition.RuntimeVersion, nameof(moduleDefinition.RuntimeVersion));
                 }
             }
