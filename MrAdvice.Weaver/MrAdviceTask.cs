aaa9b96505913c85f4954526e9c4a09fe540c407
Pascal Craponne <pascal@craponne.fr>
2016-10-16 00:35:21 +0200

    Making debug easier

--- MrAdvice.Weaver/MrAdviceTask.cs
+++ MrAdvice.Weaver/MrAdviceTask.cs
@@ -6,6 +6,8 @@
 #endregion
 
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 using ArxOne.MrAdvice;
 
@@ -20,7 +22,31 @@ public class MrAdviceTask : StitcherTask<MrAdviceStitcher>
     public static int Main(string[] args)
     {
         SetupBlobber();
-        return Run(new MrAdviceTask(), args);
+        return Run(new MrAdviceTask(), LoadArgs(args));
+    }
+
+    private static string[] LoadArgs(string[] args)
+    {
+        if (args.Length == 1 && args[0].StartsWith("@"))
+        {
+            using (var fileStream = File.OpenText(args[0].Substring(1)))
+            {
+                var cwd = fileStream.ReadLine();
+                Directory.SetCurrentDirectory(cwd);
+                var lines = new List<string>();
+                for (;;)
+                {
+                    var line = fileStream.ReadLine();
+                    if (line == null)
+                        break;
+                    if (line == "")
+                        continue;
+                    lines.Add(line);
+                }
+                return lines.ToArray();
+            }
+        }
+        return args;
     }
 
     private static Type GetLoader() => Assembly.GetExecutingAssembly().GetType("\u2302");
