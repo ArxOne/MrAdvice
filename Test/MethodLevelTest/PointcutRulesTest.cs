42054a476883cc712397522ac21c0176797217e1
Pascal Craponne <pascal@craponne.fr>
2016-11-06 10:18:11 +0100

    Implemented test showing my miserable failure

--- Test/MethodLevelTest/PointcutRulesTest.cs
+++ Test/MethodLevelTest/PointcutRulesTest.cs
@@ -10,7 +10,9 @@ namespace MethodLevelTest
     using System;
     using ArxOne.MrAdvice.Advice;
     using ArxOne.MrAdvice.Annotation;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+    [TestClass]
     public class PointcutRulesTest
     {
         [IncludePointcut("*.Set@")]
@@ -28,6 +30,17 @@ namespace MethodLevelTest
             public void SetA() { }
             public void ZetA() { }
             public void SetB() { }
+        }
+
+        [TestMethod]
+        [TestCategory("Pointcut selection")]
+        public void SetterRulesTest()
+        {
+            var t = typeof(SetterAdvisedType);
+            Assert.IsNull(ArxOne.MrAdvice.Advices.Get(t.GetConstructor(new Type[0])));
+            Assert.IsNull(ArxOne.MrAdvice.Advices.Get(t.GetMethod(nameof(SetterAdvisedType.ZetA))));
+            Assert.IsNotNull(ArxOne.MrAdvice.Advices.Get(t.GetMethod(nameof(SetterAdvisedType.SetA))));
+            Assert.IsNotNull(ArxOne.MrAdvice.Advices.Get(t.GetMethod(nameof(SetterAdvisedType.SetB))));
         }
     }
 }
