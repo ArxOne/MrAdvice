448422dc88ffa3e077efb4139e68423f47a7ff85
Seyyed Soroosh Hosseinalipour <soorosh_abi@hotmail.com>
2020-01-12 23:29:36 +0330

    Add basic test for dotnet build.

---
+++ Test/NetStandradTest/BasicTest.cs
@@ -0,0 +1,25 @@
+﻿#region Mr. Advice
+
+// Mr. Advice
+// A simple post build weaving package
+// http://mradvice.arxone.com/
+// Released under MIT license http://opensource.org/licenses/mit-license.php
+
+#endregion
+
+namespace NetStandardTest
+{
+	using ArxOne.MrAdvice.Advice;
+	using System;
+
+	public class BasicTestAdvice : Attribute, IMethodAdvice
+	{
+		public BasicTestAdvice(bool validateAllProperties = true)
+		{
+		}
+
+		public void Advise(MethodAdviceContext context)
+		{
+		}
+	}
+}
