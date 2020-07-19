8991d8f817eee95f20fcb845bed9b702cabd593f
Pascal Craponne <pascal@craponne.fr>
2020-07-19 17:46:16 +0200

    Release 2.8.11

--- MrAdvice/Properties/ProductInfo.cs
+++ MrAdvice/Properties/ProductInfo.cs
@@ -19,7 +19,7 @@ using ArxOne.MrAdvice.Properties;
 [assembly: NeutralResourcesLanguage("en")]
 
 [assembly: AssemblyVersion("2")]
-[assembly: AssemblyFileVersion("2.8.10")] // 
+[assembly: AssemblyFileVersion("2.8.11")] // 
 
 namespace ArxOne.MrAdvice.Properties
 {
@@ -31,6 +31,6 @@ namespace ArxOne.MrAdvice.Properties
         /// <summary>
         /// The version
         /// </summary>
-        public const string Version = "2.8.10";
+        public const string Version = "2.8.11";
     }
 }
